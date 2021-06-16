3d319ff62befdd6285dbf2377e0b1f5ff3290463
khai <KhaiZhang@163.com>
2021-06-16 23:36:34 +0800

    [Khai][add test][should print all transaction when account print statement]

--- src/test/java/com/tw/banking/AccountTest.java
+++ src/test/java/com/tw/banking/AccountTest.java
@@ -3,6 +3,13 @@ package com.tw.banking;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 
+import java.time.LocalDateTime;
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.mock;
+
 class AccountTest {
 
     @Test
@@ -33,5 +40,22 @@ class AccountTest {
 
         //then
         Mockito.verify(spyTxnRepo).addWithdraw(100);
+    }
+
+    @Test
+    void should_print_all_transaction_when_account_print_statement() {
+        //given
+        Printer printer = mock(Printer.class);
+        TransactionRepository transactionRepository = mock(TransactionRepository.class);
+        Account account = new Account(transactionRepository, printer);
+        List<Transaction> allTransactionList = Arrays.asList(new Transaction(LocalDateTime.now().toString(), 1000));
+        Mockito.when(transactionRepository.allTransactions()).thenReturn(allTransactionList);
+        Mockito.doNothing().when(printer).print(allTransactionList);
+
+        //when
+        account.printStatement();
+
+        //then
+        Mockito.verify(printer).print(allTransactionList);
     }
 }

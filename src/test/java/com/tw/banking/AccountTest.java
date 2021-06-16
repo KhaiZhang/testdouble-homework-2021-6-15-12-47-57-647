2d47020c7dff5c312f5e4d8962d46a3f597eee9b
khai <KhaiZhang@163.com>
2021-06-16 23:16:51 +0800

    [Khai][add test][should deposit when account add amount]

---
+++ src/test/java/com/tw/banking/AccountTest.java
@@ -0,0 +1,22 @@
+package com.tw.banking;
+
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+
+class AccountTest {
+
+    @Test
+    void should_deposit_when_account_add_amount() {
+        //given
+        Printer printer = new Printer(new Console());
+        TransactionRepository transactionRepository = new TransactionRepository(new Clock());
+        TransactionRepository spyTxnRepo = Mockito.spy(transactionRepository);
+        Account account = new Account(spyTxnRepo, printer);
+
+        //when
+        account.deposit(100);
+
+        //then
+        Mockito.verify(spyTxnRepo).addDeposit(100);
+    }
+}

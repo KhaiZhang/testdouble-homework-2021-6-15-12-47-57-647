e05690352eddd388b896c01ef66638b79373df17
khai <KhaiZhang@163.com>
2021-06-17 00:00:29 +0800

    [Khai][add test][should_get_all_transaction_when_transaction_repository_get_all_transaction]

--- src/test/java/com/tw/banking/TransactionRepositoryTest.java
+++ src/test/java/com/tw/banking/TransactionRepositoryTest.java
@@ -4,6 +4,8 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 
+import java.util.List;
+
 import static org.mockito.Mockito.mock;
 
 class TransactionRepositoryTest {
@@ -36,5 +38,22 @@ class TransactionRepositoryTest {
         //then
         Assertions.assertEquals("06/16/2021", transactionRepository.allTransactions().get(0).date());
         Assertions.assertEquals(-100, transactionRepository.allTransactions().get(0).amount());
+    }
+
+    @Test
+    void should_get_all_transaction_when_transaction_repository_get_all_transaction() {
+        //given
+        Clock mockClock = mock(Clock.class);
+        TransactionRepository transactionRepository = new TransactionRepository(mockClock);
+        Mockito.when(mockClock.todayAsString()).thenReturn("06/16/2021");
+
+        //when
+        transactionRepository.addDeposit(100);
+        List<Transaction> actualTransactions = transactionRepository.allTransactions();
+
+        //then
+        Assertions.assertTrue(actualTransactions.size() == 1);
+        Assertions.assertEquals("06/16/2021", transactionRepository.allTransactions().get(0).date());
+        Assertions.assertEquals(100, transactionRepository.allTransactions().get(0).amount());
     }
 }
